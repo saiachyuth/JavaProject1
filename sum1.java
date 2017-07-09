2791c0b81a011c3adc5155bbee0e3b7c61461a68
saiachyuth <brahmadevamsaia_brahma@srmuniv.edu.in>
2017-07-09 15:52:05 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sum1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x1,i;
+	  int s1=0;
+	 Scanner s= new Scanner(System.in);
+	 x1=s.nextInt();
+	 for(i=1;i<=x1;i++)
+	 s1=s1+i;
+	 System.out.println("sum:"+s1);
+  	}
+}
