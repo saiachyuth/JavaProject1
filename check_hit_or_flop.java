e17ea2d4dd22fb4f49160aa6d399e1a5dc2e767f
saiachyuth <brahmadevamsaia_brahma@srmuniv.edu.in>
2017-07-09 11:43:38 +0530

    Add files via upload

---
+++ check_hit_or_flop.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+public class check_hit_or_flop;
+{
+	public static void main (String[] args) 
+    {
+	int a;
+    scanner s =new scanner(system.in);
+    system.out.println('no.of days');
+    c=s.nextInt();   
+    if(a>=50) {
+    system.out.println('box office hit'); }
+    else {
+    system.out.println('flop'); }
+	}
+}
